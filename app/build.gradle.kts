7de2b7ba80e61a231813f0fc9cf7928cea6e74d7
scroodge <scroodgemac@gmail.com>
2026-08-26 10:46:27 +0300

    chore: bump version to 0.5.4 (341)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         // on DiLink Android 12 (requestLegacyExternalStorage works).
         // targetSdk 30+ would break listFiles() on /storage/emulated/0/energydata/
         targetSdk = 29
-        versionCode = 340
-        versionName = "0.5.3"
+        versionCode = 341
+        versionName = "0.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
